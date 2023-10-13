5148719d20ce2a328fa224eec7735e61a7f6379f
sravaniNuthi <sravani.nuthi@outlook.com>
2023-10-12 21:35:28 -0500

    Latest - Registrationa nd Login

--- js/username_match.js
+++ js/username_match.js
@@ -18,3 +18,50 @@ document.getElementById("loginButton").addEventListener("click", function() {
         console.error("Error fetching users:", error);
     });
 });
+
+document.getElementById("registrationButton").addEventListener("click", function() {
+    var enteredUsername = document.querySelector(".registration input[type=text]").value;
+    var enteredPassword = document.querySelector(".registration input[type=password]").value;
+    var confirmPassword = document.querySelector(".registration input[type=password][placeholder='Confirm your password']").value;
+
+    if (enteredPassword !== confirmPassword) {
+        alert("Password and confirm password do not match. Please try again.");
+        return;
+    }
+
+    fetch('users.json')
+        .then(response => response.json())
+        .then(data => {
+            // Check if the entered username already exists
+            var userExists = data.users.some(u => u.username === enteredUsername);
+
+            if (userExists) {
+                alert("Username already exists. Please choose a different one.");
+            } else {
+                // Add the new user to the list and update the JSON
+                data.users.push({ username: enteredUsername, password: enteredPassword });
+
+                // Convert the updated data back to a JSON string
+                var updatedData = JSON.stringify(data);
+
+                // Save the updated data to the users.json file
+                fetch('users.json', {
+                    method: 'PUT',
+                    body: updatedData,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then(() => {
+                        // Redirect to the "Redirected" page after registration
+                        window.location.href = "redirect.html";
+                    })
+                    .catch(error => {
+                        console.error("Error saving user data:", error);
+                    });
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching users:", error);
+        });
+});
